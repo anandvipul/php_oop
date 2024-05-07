4ef8b50d92606305302713ef4e6297d1ea479b85
anandvipul <anandvipul01@gmail.com>
2024-05-08 00:30:30 +0530

    php_abstract_classes

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
 
+abstract class Car {
+    public $name;
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    abstract public function intro() :string;
+}
+
+class Audi extends Car {
+    public function intro() :string {
+        return "I am Audi";
+    }
+}
+
+class Volvo extends Car {
+    public function intro() :string {
+        return "i am Volvo";
+    }
+}

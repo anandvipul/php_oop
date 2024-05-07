43e562de6e4ec412ad0a94db61c70f1c3dcbdf4c
anandvipul <anandvipul01@gmail.com>
2024-05-07 22:15:20 +0530

    php_constructor

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 
+class Fruit {
+    public $name;
+    public $color;
+
+    function __construct(string $name, string $color) {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+
+    function get_color() {
+        return $this->color;
+    }
+}
+
+
+$apple = new Fruit("Apple", "Red");
+
+echo $apple->get_name();

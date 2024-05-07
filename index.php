7dccc540096061241aa4ca91ac823d56d205805d
anandvipul <anandvipul01@gmail.com>
2024-05-07 23:48:34 +0530

    php_constants

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
 
+class GoodBye {
+    const LEAVING_MESSAGE = "Thank You for Visiting";
+    public function message() {
+        echo self::LEAVING_MESSAGE;
+    }
+}
+
+echo GoodBye::LEAVING_MESSAGE;
+
+$goodbye = new GoodBye();
+$goodbye->message();

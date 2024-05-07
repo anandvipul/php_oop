3fc54495ebe886fd0e41fed5fb6dccf76d8ed632
anandvipul <anandvipul01@gmail.com>
2024-05-07 22:45:56 +0530

    php_access_modifiers

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 
+class Fruit {
+    public $name;
+    public $color;
+    public $weight;
+
+    function set_name($name) {
+        $this->name = $name;
+    }
+
+    protected function set_color($color) {
+        $this->color = $color;
+    }
+
+    private function set_weight($weight) {
+        $this->weight = $weight;
+    }
+}
+
+$apple = new Fruit();
+$apple->set_name("Apple");
+$apple->set_color("Red");
+$apple->set_weight("5.8");

f17650e80b9bef550f2f45d26c864d8fcd01a51a
anandvipul <anandvipul01@gmail.com>
2024-05-07 22:28:41 +0530

    php_destructor

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
 
+class Fruit {
+    public $name;
+    public $color;
+
+    function __construct($name, $color) {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+
+    function __destruct() {
+        echo "The fruit is $this->name and its color is $this->color.";
+    }
+}
+
+$apple = new Fruit("Apple", "Red");

868157a4df26723e80ad40501362cd64bdac83b9
anandvipul <anandvipul01@gmail.com>
2024-05-08 12:59:54 +0530

    With other class

--- index.php
+++ index.php
@@ -4,6 +4,26 @@ class greetings {
     public static function welcome() {
         echo "hello";
     }
+
+    public function __construct() {
+        self::welcome();
+    }
 }
 
 greetings::welcome();
+
+class ClassA {
+    public static function messageA() {
+        return "Hello From Class A";
+    } 
+}
+
+class ClassB {
+
+    
+    public function __destruct() {
+        echo ClassA::messageA();
+    }
+}
+
+$classB = new ClassB();

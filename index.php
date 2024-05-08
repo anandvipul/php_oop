6ff0679f12ad704b5239a79d6f8500b5dbfc93f3
anandvipul <anandvipul01@gmail.com>
2024-05-08 10:17:59 +0530

    php_interfaces

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
 
+interface Animal {
+    public function makeSound();
+}
+
+class Cat implements Animal {
+    public function makeSound() {
+        echo "Meow";
+    }
+}
+
+class Dog implements Animal {
+    public function makeSound() {
+        echo "Bhow";
+    }
+}
+
+$cat = new Cat();
+$dog = new Dog();
+
+$animals = array($cat, $dog);
+
+foreach($animals as $animal) {
+    $animal -> makeSound();
+}
